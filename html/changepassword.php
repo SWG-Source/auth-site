547322b8c30c709572fa7e2d367b91a029936422
Bandit42 <35357152+Bandit42@users.noreply.github.com>
2020-04-06 09:53:00 -0700

    modified changepassword.php and form_login.php
    Added code for backgrounds and modified text for visibility to changepassword.php and form_login.php

--- html/changepassword.php
+++ html/changepassword.php
@@ -10,11 +10,77 @@
 		<meta name = "viewport" content = "width = device-width, initial-scale = 1, minimum-scale=1, maximum-scale=1, user-scalable=no">
 			<title>Change Password</title>
 	</head>
-	<body>
+	<style>
+	div.container {
+    width: 90%;
+    border: none;
+}
+
+header, footer {
+    padding: 1em;
+    color: white;
+    background-color: none;
+    clear: left;
+    text-align: center;
+}
+
+nav {
+    float: left;
+    max-width: 160px;
+    margin: 0;
+    padding: 1em;
+}
+
+nav ul {
+    list-style-type: none;
+    padding: 0;
+}
+   
+nav ul a {
+    text-decoration: none;
+}
+
+article {
+    margin-left: 170px;
+    border-left: none;
+    padding: 1em;
+    overflow: hidden;
+}
+body  {
+    background-color: black;
+    background-image: url("/images/grevious.jpg");
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-position: center;
+    background-size: cover;
+}
+a:link {
+    color: green;
+    background-color: transparent;
+    text-decoration: none;
+}
+a:visited {
+    color: orange;
+    background-color: transparent;
+    text-decoration: none;
+}
+a:hover {
+    color: red;
+    background-color: transparent;
+    text-decoration: underline;
+}
+a:active {
+    color: yellow;
+    background-color: transparent;
+    text-decoration: underline;
+}    
+</style>
+	<center>
+		<body>
 		<form action='post_changepassword.php' method='post' border='0'>
 		<table>
 			<tr>
-				<td>Username</td>
+				<td><b><p style="color:white;">Username</p></b></td>
 				<td><select name="username">
 <?php
 			include 'includes/db_connect.php';
@@ -40,7 +106,7 @@
 			</select></td>
 		</tr>
 		<tr>
-			<td>New Password</td>
+			<td><b><p style="color:white;">New Password</p></b></td>
 			<td><input id='realpassword' type='realpassword' name='password' /></td>
 		</tr>
 		<tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr>
@@ -54,4 +120,5 @@
 		</table>
 		</form>
 		</body>
+	</center>
 </html>
--- html/form_login.php
+++ html/form_login.php
@@ -6,11 +6,76 @@ session_start();
 <meta name = "viewport" content = "width = device-width">
 <meta name = "viewport" content = "initial-scale = 1.0">
 </head>
+<style>
+	div.container {
+    width: 90%;
+    border: none;
+}
+
+header, footer {
+    padding: 1em;
+    color: white;
+    background-color: none;
+    clear: left;
+    text-align: center;
+}
+
+nav {
+    float: left;
+    max-width: 160px;
+    margin: 0;
+    padding: 1em;
+}
+
+nav ul {
+    list-style-type: none;
+    padding: 0;
+}
+   
+nav ul a {
+    text-decoration: none;
+}
+
+article {
+    margin-left: 170px;
+    border-left: none;
+    padding: 1em;
+    overflow: hidden;
+}
+body  {
+    background-color: black;
+    background-image: url("/images/mfalcon.jpg");
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-position: center;
+    background-size: cover;
+}
+a:link {
+    color: green;
+    background-color: transparent;
+    text-decoration: none;
+}
+a:visited {
+    color: orange;
+    background-color: transparent;
+    text-decoration: none;
+}
+a:hover {
+    color: red;
+    background-color: transparent;
+    text-decoration: underline;
+}
+a:active {
+    color: yellow;
+    background-color: transparent;
+    text-decoration: underline;
+}    
+</style>
 <body>
 <center><img src="images/swgsource.png" alt="" width=200/></center>
 <form method="post" action="post_login.php">
-  <center><p>Username: <input name="username" type="text"></p>
-<p>Password: <input name="password" type="password"></p>
+	<center><b><p style="color:white;">Username: <input name="username" type="text"></p></b>
+		<b><p style="color:white;">Password: <input name="password" type="password"></p></b>
 <input name="submit" type="submit" value="Submit"></center>
 </form>
 </body>
